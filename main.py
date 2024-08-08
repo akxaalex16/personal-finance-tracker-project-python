fa0abd88c16335e001e2b661e2cfaa74bf6de449
akxaalex16 <akxaalex16@gmail.com>
2024-08-07 20:23:30 -0700

    functionality to dynamically add data to the finance_data.csv file; defined get_transactions to filter by date

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import csv
 from datetime import datetime
+from data_entry import get_date, get_amount, get_category, get_description
 
 
 class CSV:
     CSV_FILE = 'finance_data.csv'
     COLUMNS = ['date', 'amount', 'category', 'description']
+    FORMAT = '%d-%m-%Y'
+
     @classmethod
     def initialize_csv(cls):
         try:
@@ -27,6 +30,33 @@ class CSV:
             writer.writerow(new_entry)
         print('Entry added successfully')
 
+    @classmethod
+    def get_transactions(cls, start_date, end_date):
+        df = pd.read_csv(cls.CSV_FILE)
+        df['date'] = pd.to_datetime(df['date'], format=CSV.FORMAT)
+        start_date = datetime.strptime(start_date, CSV.FORMAT)
+        end_date = datetime.strptime(end_date, CSV.FORMAT)
 
-CSV.initialize_csv()
-CSV.add_entry("20-07-2024", 125.65, "Income", "Salary")
+        mask = (df['date'] >= start_date) & (df['date'] <= end_date)
+        filtered_df = df.loc[mask]
+
+        if filtered_df.empty:
+            print('No transactions found in the given date range.')
+        else:
+            print(f'Transactions from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}')
+            print(filtered_df.to_string(index=False, formatters={'date': lambda x: x.strftime(CSV.FORMAT)}))
+
+
+def add():
+    CSV.initialize_csv()
+    date = get_date("Enter the date of the transaction (dd-mm-yyyy) or press enter for today's date: ", allow_default=True)
+    amount = get_amount()
+    category = get_category()
+    description = get_description()
+    CSV.add_entry(date, amount, category, description)
+
+# CSV.initialize_csv()
+# CSV.add_entry("20-07-2024", 125.65, "Income", "Salary")
+
+
+add()
